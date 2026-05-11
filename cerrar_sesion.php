744642818f6e3315d98e9dff1a687ed2a0506739
Santiago Aguilar <santiagaguilardecano@gmail.com>
2026-05-11 01:07:38 -0300

    Delete cerrar_sesion.php

--- cerrar_sesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_unset(); // Limpia las variables de sesión
-    session_destroy(); // Destruye la sesión
-
-    header("Location: ../html/login.php"); // Cambialo a .php como quedamos
-    exit();
-?>
